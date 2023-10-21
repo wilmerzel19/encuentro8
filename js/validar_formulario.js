7c4f305b58ba029e406190d0e323670b2fe24691
wilmer <est.wzelayam@uml.edu.ni>
2023-10-21 14:52:58 -0600

    login

---
+++ js/validar_formulario.js
@@ -0,0 +1,7 @@
+function login (){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    alert ("usuario: " + username + " contraseña: " + password);
+
+}
+
