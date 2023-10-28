83da928f17be2c360e5f47b1059d817e1d4f73bc
wilmer <est.wzelayam@uml.edu.ni>
2023-10-28 14:52:16 -0600

    login

--- js/validar_formulario.js
+++ js/validar_formulario.js
@@ -1,8 +1,23 @@
 function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    fetch('https://dummyjson.com/auth/login', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    
+    username: username,//'kminchelle'
+    password:password, //'0lelplR',
+    // expiresInMins: 60, // optional
+  })
+})
+.then(res => res.json())
+.then(console.log);
 
-    if (username == "admin" && password == "admin") {
+}
+
+
+    /*if (username == "admin" && password == "admin") {
 
         //definir un valor en el local storage
         localStorage.setItem("logged", "true");
@@ -14,20 +29,17 @@ function login() {
         alert("Usuario o contraseña incorrecta");
 
     }
+}*/
+function validar_login() {
+    const logged = localStorage.getItem("logged");
+    if (logged !== "true") {
+        window.location.href = "cards.html";
+    }
 }
+
 function logout() {
     localStorage.setItem("logged", "false");
     window.location.href = "cards.html";
 
 
 }
-
-function validar_login() {
-    const logged = localStorage.getItem("logged");
-    if (logged == "true") {
-        window.location.href = "cards.html";
-    }
-}
-
-
-
